7bbbf68384ccb7f2c7bfb877559b50b3c3b8f02e
ᗪᗩᖇK ✞Oᖇᗪ <85796566+Judson-web@users.noreply.github.com>
2021-11-12 13:40:57 +0530

    Create cbb.py

---
+++ cbb.py
@@ -0,0 +1,29 @@
+#(©)Judson-web
+#(©)Judson-web
+
+from pyrogram import __version__
+from bot import Bot
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text = f"<b>○ Mᴀsᴛᴇʀ : <a href='t.me/VAMPIRE_KING_NO_1'>ƬЄƦƦƠƦ MƖƇƘЄƳ</a>\n○ Lᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n○ Lɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href='https://github.com/Judson-web/File-SHare-Bot'>Click Here</a>\n○ Cʜᴀɴɴᴇʟ : @storytimeoGG\n○ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ : @STMbOTsUPPORTgROUP</b>",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🔒 Cʟᴏsᴇ", callback_data = "close")
+                    ]
+                ]
+            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
